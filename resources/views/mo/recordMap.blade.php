55a08006a384c5e29cda1d88634bfd07887cd6f0
jaehyeok <charbori@github.com>
2023-07-31 16:03:53 +0000

    mod map item list having correct image path

--- resources/views/mo/recordMap.blade.php
+++ resources/views/mo/recordMap.blade.php
@@ -256,8 +256,10 @@
 
     function initMapList(value) {
         if (typeof value == 'undefined' || value.length == 0) return $('#map_list').empty();
+        if (value.path.length < 12) value.path = '<svg xmlns="http://www.w3.org/2000/svg"  width="64" height="64" class="bi bi-card-image flex-shrink-0" fill="currentColor" viewBox="0 0 16 16"><path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/><path d="M1.5 2A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13zm13 1a.5.5 0 0 1 .5.5v6l-3.775-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12v.54A.505.505 0 0 1 1 12.5v-9a.5.5 0 0 1 .5-.5h13z"/></svg>';
+        else value.path = '<img src="' + value.path + '" alt="twbs" width="64" height="64" class="rounded-circle flex-shrink-0">'
         map_list_item = ['<a href="#" style="background-color:white; border-width:0 0 1px 0;" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true" onClick="link_map(' + value.id + ')">',
-            '<img src="https://github.com/twbs.png" alt="twbs" width="64" height="64" class="rounded-circle flex-shrink-0">',
+            value.path,
             '<div class="d-flex gap-2 w-100 justify-content-between">',
             '<div>',
             '<h6 class="mb-0">' + value.name + 'g</h6>',
